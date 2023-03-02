98a64d93edb812ba93d256b1f786bdadd2a816b7
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-03-02 16:39:22 +0100

    Initial commit

---
+++ src/main/java/be/vdab/catalogus3/Catalogus3Application.java
@@ -0,0 +1,13 @@
+package be.vdab.catalogus3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Catalogus3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Catalogus3Application.class, args);
+	}
+
+}
---
+++ src/test/java/be/vdab/catalogus3/Catalogus3ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.catalogus3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Catalogus3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
